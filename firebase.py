437b6a2660921cbde19a893cab6dd2fb5ed2cbff
MaxLin <maxylin@gmail.com>
2016-10-05 21:08:44 -0400

    Update firebase python

--- firebase.py
+++ firebase.py
@@ -9,8 +9,13 @@ config = {
 }
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
-data = {"name": "Kevin Xia"}
-db.child("users").set(data)
+data = {
+    "users": [
+        {"name": "Kevin Xia", "ratings": [0,1,2,3,4,5,6]},
+        {"name": "Max Lin", "ratings": [6,5,4,3,2,1,0]}
+    ]
+}
+db.set(data)
 #users = db.child("users").get()
 #print(users.val())
 #print(users.key())
