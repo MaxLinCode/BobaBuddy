5716ff6aa0f7576dfc91cd61b2130db654814215
MaxLin <maxylin@gmail.com>
2016-10-19 22:42:49 -0400

    Rating storage function

--- public/rate_submit.js
+++ public/rate_submit.js
@@ -1,5 +1,5 @@
 function submitRating() {
 	var form = document.getElementById("form1");
-	console.log(form.myteas.value);
+	console.log(form.myteas.option.data('id'));
 	console.log(form.rating.value);
 }
